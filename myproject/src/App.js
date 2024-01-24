bbf386d18121b2bb42bcce45fcb3dfd917930157
DanielLobo2025 <dzl2@njit.edu>
2024-01-23 17:13:33 -0800

    Class component

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Hello from './components/Hello';
+import Message from './components/Message';
 function App() {
   return (
     <div className="App">
       <Hello/>
+      <Message/>
     </div>
   );
 }
--- myproject/src/components/Hello.js
+++ myproject/src/components/Hello.js
@@ -1,6 +1,6 @@
-function Hello(){
+/*function Hello(){
     return <h1>Hello World!</h1>
 }
-
-
+*/
+const Hello = ()=> <h1>Hello There!</h1>
 export default Hello;
