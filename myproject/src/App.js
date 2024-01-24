8e108a90131d666a2ae726632de90fa9934d9a74
DanielLobo2025 <dzl2@njit.edu>
2024-01-24 06:54:34 -0800

    Added Comments

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
-import Hello from './components/Hello';
+import './App.css';//Importing App.css into App.js
+import Hello from './components/Hello';//Allows us to use the hello component in the app.js file
+//import (whatever the component is called) from '(the file path)'
 import Message from './components/Message';
+//Another example of importing a component into the app.js file
 function App() {
   return (
     <div className="App">
+      {/*FYI this is how you create a comment in react */}
       <Hello/>
+      {/*This is how you call the function you could also do <Hello><Hello/> or you can just do what I did just one tag */}
+      
       <Message/>
+      {/*Same concept <(Component name)/>) */}
     </div>
   );
 }
